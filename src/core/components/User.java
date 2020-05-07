0ac383c3d38f19a8c2809e6afbbc7815806cf39f
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-07 21:18:46 +0200

    Added user fields

--- src/core/components/User.java
+++ src/core/components/User.java
@@ -1,4 +1,24 @@
 package core.components;
 
+
 public class User {
+    private final int id = generateUniqueID();
+    private static int numberOfUsers = 0;
+
+    private int generateUniqueID() {
+        return ++numberOfUsers;
+    }
+
+    private String name;
+    private String lastName;
+    private double latitude;
+    private double longitude;
+    private Card userCard;
+    private int creditCardNumber;
+    private int registrationCard;
+    private float timeCreditBalance = 0;  // [minutes]
+    private float allMoneyCharged = 0;
+    private float currentMoneyCharged;
+
+
 }
