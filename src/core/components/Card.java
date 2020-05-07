121cd51d89c360efc7527f78c18363a5ff5960bc
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-07 22:34:35 +0200

    Made Card id final

--- src/core/components/Card.java
+++ src/core/components/Card.java
@@ -1,7 +1,7 @@
 package core.components;
 
 public abstract class Card {
-    private int id = generateUniqueID();
+    private final int id = generateUniqueID();
     private static int numberOfCards = 0;
 
     private int generateUniqueID() {
