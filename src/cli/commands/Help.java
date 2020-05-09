6a78ac65b21a75629dde8361054766f137736fd1
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-10 00:54:33 +0200

    Changed the help command

--- src/cli/commands/Help.java
+++ src/cli/commands/Help.java
@@ -1,4 +1,13 @@
 package cli.commands;
 
 public class Help implements Command {
+    /**
+     *
+     * help command
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("The help command!");
+    }
 }
