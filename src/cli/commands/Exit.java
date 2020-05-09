04e3953dd4b1c6f7c90258cd27d4a2beb7915a52
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-10 00:52:20 +0200

    Changed the exit command

--- src/cli/commands/Exit.java
+++ src/cli/commands/Exit.java
@@ -1,4 +1,13 @@
 package cli.commands;
 
 public class Exit implements Command{
+    /**
+     *
+     * Exit command
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("The exit command!");
+    }
 }
