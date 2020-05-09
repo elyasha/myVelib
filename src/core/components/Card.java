75a3ad22e6c3db603b95ba23677d1ad301309ef6
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-09 23:08:11 +0200

    Card is a abstract class, so no constructor

--- src/core/components/Card.java
+++ src/core/components/Card.java
@@ -4,14 +4,10 @@ public abstract class Card {
     private final int id = generateUniqueID();
     private static int numberOfCards = 0;
 
-    public Card() {
 
-    }
-    
     private int generateUniqueID() {
         return ++numberOfCards;
     }
-    
 
 
 }
