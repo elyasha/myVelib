e143698e94f8f66719839f47748507d1a42b8828
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-10 00:01:16 +0200

    Added doc for addUser command

--- src/cli/commands/AddUser.java
+++ src/cli/commands/AddUser.java
@@ -1,6 +1,14 @@
 package cli.commands;
 
 public class AddUser {
+    /**
+     *
+     * addUser <userName,cardType, velibnetworkName> :
+     * to add a user with name {userName} and card {cardType}
+     * (i.e. ``none'' if the user has no card) to a myVelib network {velibnetworkName}
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("The addUser command!");
     }
