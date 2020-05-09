94b8c5627a3d3641d0bc389ef91e9d4002c51b3d
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-09 23:43:51 +0200

    Added some stuff into main of cli

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -1,4 +1,48 @@
 package cli;
 
 public class Main {
+
+    public static void main(String[] args) {
+        if (args[0].equals("addUser")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("display")) {
+
+            // TODO: implement the command
+        } else if (args[0].equals("displayStation")) {
+
+            // TODO: implement the command
+        } else if (args[0].equals("displayUser")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("exit")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("help")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("offline")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("online")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("rentBike")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("returnBike")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("setup")) {
+            // TODO: implement the command
+
+        } else if (args[0].equals("sortStation")) {
+            // TODO: implement the command
+
+        } else {
+            System.out.println("Not the good usage of the myVelib program. Please see the documentation [myvelib help]");
+        // TODO: Learn how to use exceptions and error in Java
+        }
+
+    }
 }
