ef725c03e3a1f27a49efb864678bb44a8c8e6e6e
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-09 23:11:50 +0200

    Added getters, toString(), equals() and hashCode()

--- src/core/components/Bicycle.java
+++ src/core/components/Bicycle.java
@@ -1,5 +1,7 @@
 package core.components;
 
+import java.util.Objects;
+
 public abstract class Bicycle {
     private final int id = generateUniqueID();
     private static int numberOfBicycles = 0;
@@ -8,4 +10,31 @@ public abstract class Bicycle {
         return ++numberOfBicycles;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public static int getNumberOfBicycles() {
+        return numberOfBicycles;
+    }
+
+    @Override
+    public String toString() {
+        return "Bicycle{" +
+                "id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bicycle bicycle = (Bicycle) o;
+        return id == bicycle.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
