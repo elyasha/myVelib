70204ba15085326e8a6dba6fd04e6095cb763115
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-10 00:03:12 +0200

    Added doc for display command

--- src/cli/commands/Display.java
+++ src/cli/commands/Display.java
@@ -1,4 +1,15 @@
 package cli.commands;
 
 public class Display {
+    /**
+     *
+     * display <velibnetworkName>: to display the entire status (stations, parking bays,
+     * users) of an a myVelib network velibnetworkName.
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        System.out.println("The display command!");
+    }
 }
