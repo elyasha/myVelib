4ac9b16a69656558ab6cc0816ccab09589e959d4
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-10 00:04:42 +0200

    Added doc for displayStation command

--- src/cli/commands/DisplayStation.java
+++ src/cli/commands/DisplayStation.java
@@ -1,4 +1,15 @@
 package cli.commands;
 
 public class DisplayStation {
+    /**
+     *
+     * displayStation<velibnetworkName, stationID> : to display the statistics (as of
+     * Section 2.4) of station stationID of a myVelib network velibnetwork.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("The displayStation command!");
+
+    }
 }
