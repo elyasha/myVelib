00a67ae3cf493ffcff9086aa8771c71850dcb42c
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-21 01:40:59 +0200

    Added classpath, changed gitignore and added another empty test

---
+++ src/core/components/BicycleTest.java
@@ -0,0 +1,12 @@
+package core.components;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BicycleTest {
+
+    @Test
+    void getNumberOfBicycles() {
+    }
+}
