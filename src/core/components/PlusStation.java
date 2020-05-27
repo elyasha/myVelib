6d2acce009a028dfcac1245db9ee7348f5b0e972
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-27 20:11:06 +0200

    Changed the PlusStation and StandardStation

--- src/core/components/PlusStation.java
+++ src/core/components/PlusStation.java
@@ -2,10 +2,6 @@ package core.components;
 
 public class PlusStation extends Station {
 
-	public PlusStation(int numberOfParkingSlots, float latitude, float longitude, boolean status) {
-	    this.setNumberOfParkingSlots(numberOfParkingSlots);
-	    this.setPosition(latitude, longitude);
-	    this.setStatus(status);
+	public PlusStation() {
 	}
-
 }
--- src/core/components/StandardStation.java
+++ src/core/components/StandardStation.java
@@ -2,10 +2,7 @@ package core.components;
 
 public class StandardStation extends Station {
 
-	public StandardStation(int numberOfParkingSlots, float latitude, float longitude, boolean status) {
-		this.setNumberOfParkingSlots(numberOfParkingSlots);
-		this.setPosition(latitude, longitude);
-		this.setStatus(status);
+	public StandardStation() {
 	}
 
 }
