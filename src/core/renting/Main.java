5f1c24449ade6ec63c744ddf61ce5759d9b64109
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-27 20:10:48 +0200

    Create the getCost methods in renting\Main

--- src/core/renting/Main.java
+++ src/core/renting/Main.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public class Main {
 
-    public double getCost(Bicycle bicycle, Card card, Time intervalOfTime) {
+    public double getCostWithoutTimeCredit(Bicycle bicycle, Card card, Time intervalOfTime) {
 
         double cost = 0;
 
@@ -55,6 +55,43 @@ public class Main {
         }
         return cost;
     }
+
+    public double getCostWithTimeCredit(Bicycle bicycle, Card card, Time intervalOfTime, double timeCredit, User user){
+
+        double cost = 0;
+        double hours;
+
+        if (timeCredit < 0) {
+            System.out.println("ERROR ! TIME CREDIT MUST BE POSITIVE");
+        }
+        if (timeCredit == 0) {
+            cost = getCostWithoutTimeCredit(bicycle,card,intervalOfTime);
+        }
+        else {
+            if (intervalOfTime.getHours()*60 + intervalOfTime.getMinutes() - timeCredit > 0) {
+                hours = (intervalOfTime.getHours()*60 + intervalOfTime.getMinutes() - timeCredit)/60;
+
+                cost = getCostWithoutTimeCredit(bicycle, card, new Time((int) hours, 0, 0));
+
+                user.setTimeCreditBalance(0);
+
+            }
+            else if (intervalOfTime.getHours()*60 + intervalOfTime.getMinutes() - timeCredit == 0) {
+                cost = 0; //
+                // the new timeCredit = 0
+                user.setTimeCreditBalance(0);
+
+            }
+            else if (intervalOfTime.getHours()*60 + intervalOfTime.getMinutes() - timeCredit < 0) {
+                cost = 0;
+                // the new timeCredit < the old timeCredit
+                user.setTimeCreditBalance((float) (timeCredit - (intervalOfTime.getHours()*60 + intervalOfTime.getMinutes())));
+            }
+
+        }
+        return cost;
+    }
+
 
 
 }
