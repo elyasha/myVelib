9bb6a1b63b2d21132b832e00c9dba25a441cb52f
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-27 14:10:49 +0200

    Update terminal

--- src/core/components/Terminal.java
+++ src/core/components/Terminal.java
@@ -1,29 +1,50 @@
 package core.components;
 
-/**
- * Singleton class. Eagerly initialized static instance guarantees thread safety.
- */
 public final class Terminal {
-    // singleton pattern
 
-    /**
-     * Private constructor so nobody can instantiate the class.
-     */
-    private Terminal() {
+    private boolean onService;
+
+    public Terminal() {
+        this.onService = false; // It won't work by default
     }
 
-    /**
-     * Static to class instance of the class.
-     */
-    private static final Terminal INSTANCE = new Terminal();
+    public Terminal(boolean onService) {
+        this.onService = onService;
+    }
 
-    /**
-     * To be called by user to obtain instance of the class.
-     *
-     * @return instance of the singleton.
-     */
-    public static Terminal getInstance() {
-        return INSTANCE;
+
+    // Getters and setters
+
+    public boolean isOnService() {
+        return onService;
+    }
+
+    public void setOnService(boolean onService) {
+        this.onService = onService;
+    }
+
+
+    // Written methods
+
+    public void rentBicycle(Bicycle bicycle, User user) {
+        // Planning checks if there is a bicycle, we don't need to check again
+
+        // If there is a bicycle available
+        System.out.println("Here you go!" + user);
+        if (bicycle instanceof ElectricalBicycle) {
+            System.out.println("A electrical bicycle!");
+        } else if (bicycle instanceof MechanicalBicycle) {
+            System.out.println("A mechanical bicycle!");
+        }
+
+    }
+
+    public void dropBicycle(Bicycle bicycle, User user) {
+        // No need to check if there is a free spot
+
+
+        // Return bicycle
+        System.out.println(user + "You rent is finalized!");
     }
 
 }
