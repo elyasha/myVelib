0c46759edac509f548202518fcec71e824350d9b
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-27 13:41:24 +0200

    Added singleton base to terminal

--- src/core/components/Terminal.java
+++ src/core/components/Terminal.java
@@ -1,4 +1,29 @@
 package core.components;
 
-public class Terminal {
+/**
+ * Singleton class. Eagerly initialized static instance guarantees thread safety.
+ */
+public final class Terminal {
+    // singleton pattern
+
+    /**
+     * Private constructor so nobody can instantiate the class.
+     */
+    private Terminal() {
+    }
+
+    /**
+     * Static to class instance of the class.
+     */
+    private static final Terminal INSTANCE = new Terminal();
+
+    /**
+     * To be called by user to obtain instance of the class.
+     *
+     * @return instance of the singleton.
+     */
+    public static Terminal getInstance() {
+        return INSTANCE;
+    }
+
 }
