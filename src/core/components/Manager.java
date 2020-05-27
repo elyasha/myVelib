26b74e03abf55d9263d7f5695d9cffe9d809a622
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-27 20:09:54 +0200

    Updated Manager.md

--- src/core/components/Manager.java
+++ src/core/components/Manager.java
@@ -1,13 +1,11 @@
 package core.components;
+import java.awt.*;
 import java.util.List;
 //import java.awt.List;
 import java.util.ArrayList;
 
 public class Manager extends User {
-	
-	
-	public List<Station> getAllStations() {
-		List<Station> allStations;
-		return null;
+	public Manager(String name, String lastName, Point coordinate, int creditCardNumber) {
+		super(name, lastName, coordinate, creditCardNumber);
 	}
 }
