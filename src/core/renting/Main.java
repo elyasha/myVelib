2e7224c835c1cc2618aa9dcc166760417d78ddd0
Unknown <ch.baudinet@gmail.com>
2020-05-27 15:45:39 +0200

    Correction Matheus errors

--- src/core/renting/Main.java
+++ src/core/renting/Main.java
@@ -8,56 +8,55 @@ import java.util.Date;
 
 public class Main {
 
-    public double getCost(Bicycle bicycle, Card card, Time intervalOfTime) {
+	public double getCost(Bicycle bicycle, Card card, Time intervalOfTime) {
 
-        double cost = 0;
+		double cost = 0;
 
-        if (bicycle instanceof ElectricalBicycle) {
-            if (card instanceof Vlibre) {
-                if (intervalOfTime.getHours() >= 1) {
-                    cost += 2;
-                }
-                else {
-                    cost += 1; // TODO: Learn know to import CSV file
-                }
+		if (bicycle instanceof ElectricalBicycle) {
+			if (card instanceof Vlibre) {
+				if (intervalOfTime.getHours() >= 1) {
+					cost = 1 + (intervalOfTime.getHours() * 2);
+				} else {
+					cost = 1; // TODO: Learn know to import CSV file
+				}
 
-            } else if (card instanceof Vmax) {
-                if (intervalOfTime.getHours() >= 1) {
-                    return 1;
-                }
-                return 0;
-            } else {
-                return 2;
-            }
-        }
+			} else if (card instanceof Vmax) {
+				if (intervalOfTime.getHours() >= 1) {
+					cost = (intervalOfTime.getHours() * 1);
+				}
+				else {
+					cost = 0;
+				}
+				
+			} else {
+				cost = intervalOfTime.getHours() * 2;
+			}
+		}
 
-        else if (bicycle instanceof MechanicalBicycle) {
-            if (card instanceof Vlibre) {
-                if (intervalOfTime.getHours() >= 1) {
-                    return 1;
-                }
-                return 0; // TODO: Learn know to import CSV file
+		else if (bicycle instanceof MechanicalBicycle) {
+			if (card instanceof Vlibre) {
+				if (intervalOfTime.getHours() >= 1) {
+					cost = (intervalOfTime.getHours() * 1);
+				}
+				else {
+					cost = 0; // TODO: Learn know to import CSV file
+				}
+				
+			} else if (card instanceof Vmax) {
+				if (intervalOfTime.getHours() >= 1) {
+					cost = (intervalOfTime.getHours() * 1);
+				}
+				else {
+					cost = 0; // TODO: Learn know to import CSV file
+				}
+				
+			}
 
-            } else if (card instanceof Vmax) {
-                if (intervalOfTime.getHours() >= 1) {
-                    return 1;
-                }
-                return 0;
-            } else {
-                return 1;
-            }
+		}
+		return cost;
+	}
 
-            return cost;
-        }
-
-        else {
-            return -9999; // TODO: See if we can make this better
-        }
-
-
-    }
-
-    public double getCost(Bicycle bicycle, Card card, Time intervalOfTime) {
+	public double getCost(Bicycle bicycle, Card card, Time intervalOfTime) {
         double cost = intervalOfTime.getHours()*
     }
 
