53354b1a9ab5007a32475c690b3e52f8566f49e8
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-29 22:02:27 +0200

    Finished bicycle factory

--- src/core/components/factories/BicycleFactory.java
+++ src/core/components/factories/BicycleFactory.java
@@ -1,4 +1,43 @@
 package core.components.factories;
 
+import core.components.ElectricalBicycle;
+import core.components.MechanicalBicycle;
+import core.components.ParkingSlot;
+import core.components.Station;
+import core.system.MyVelibSystem;
+
 public class BicycleFactory {
+
+    public void addElectricalBicycle(MyVelibSystem system, Station station) {
+
+        // Check the station (parkingSlot available)
+        ParkingSlot freeSlot = station.getOneFreeSlot();
+        if (freeSlot != null) {
+            // Creating the bicycle
+            ElectricalBicycle bicycle = new ElectricalBicycle();
+
+            // Add to station
+            freeSlot.setState(1);
+            freeSlot.setBike(bicycle);
+        } else {
+            System.out.println("There is no available slot!");
+        }
+
+
+    }
+
+    public void addMechanicalBicycle(MyVelibSystem system, Station station) {
+        // Check the station (parkingSlot available)
+        ParkingSlot freeSlot = station.getOneFreeSlot();
+        if (freeSlot != null) {
+            // Creating the bicycle
+            MechanicalBicycle bicycle = new MechanicalBicycle();
+
+            // Add to station
+            freeSlot.setState(1);
+            freeSlot.setBike(bicycle);
+        } else {
+            System.out.println("There is no available slot!");
+        }
+    }
 }
