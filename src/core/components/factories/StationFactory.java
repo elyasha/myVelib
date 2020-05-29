2d8f9e6aee5fb1623de425781fc2a793f856ae2d
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-29 23:37:27 +0200

    Update project

--- src/core/components/factories/StationFactory.java
+++ src/core/components/factories/StationFactory.java
@@ -7,33 +7,42 @@ import java.awt.*;
 import java.util.List;
 
 public class StationFactory {
+    // TODO: Create a test to check if there is already a station when creating one
 
-    public void addPlusStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots, double money, int numberOfLocations) {
+    public static Station addPlusStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots, double money, int numberOfLocations) {
         PlusStation station = new PlusStation(coordinate, onService, parkingSlots, money, numberOfLocations);
         List<Station> stations = system.getStations();
         stations.add(station);
         system.setStations(stations);
+
+        return station;
     }
 
-    public void addPlusStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots) {
+    public static Station addPlusStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots) {
         PlusStation station = new PlusStation(coordinate, onService, parkingSlots);
         List<Station> stations = system.getStations();
         stations.add(station);
         system.setStations(stations);
+
+        return station;
     }
 
-    public void addStandardStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots, double money, int numberOfLocations) {
+    public static Station addStandardStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots, double money, int numberOfLocations) {
         StandardStation station = new StandardStation(coordinate, onService, parkingSlots, money, numberOfLocations);
         List<Station> stations = system.getStations();
         stations.add(station);
         system.setStations(stations);
+
+        return station;
     }
 
-    public void addStandardStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots) {
+    public static Station addStandardStation(MyVelibSystem system, Point coordinate, boolean onService, List<ParkingSlot> parkingSlots) {
         StandardStation station = new StandardStation(coordinate, onService, parkingSlots);
         List<Station> stations = system.getStations();
         stations.add(station);
         system.setStations(stations);
+
+        return station;
     }
 
 }
--- src/core/setup/InitialSetup.java
+++ src/core/setup/InitialSetup.java
@@ -1,20 +1,30 @@
 package core.setup;
 
-import core.components.User;
+import core.components.*;
 import core.components.factories.CardFactory;
+import core.components.factories.ParkingSlotsFactory;
+import core.components.factories.StationFactory;
 import core.components.factories.UserFactory;
 import core.system.MyVelibSystem;
 import core.system.MyVelibSystemFactory;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InitialSetup {
 
     private static final double STATION_OCCUPIED_PERCENTAGE = 0.7;
     private static final double PERCENTAGE_OF_ELECTRICAL_BICYCLE = 0.3;
+    private static final double PERCENTAGE_OF_PLUS_STATIONS = 0.5;
 
     public static void main(String[] args) {
+
+        // Creation of the myVelib system
+        MyVelibSystem system0 = MyVelibSystemFactory.createMyVelibSystem();
 
         // Name of the myVelib system
         String name = args[0];
@@ -28,8 +38,22 @@ public class InitialSetup {
         // Station occupied percentage
         double stationOccupiedPercentage = STATION_OCCUPIED_PERCENTAGE;
 
+        int numberOfOccupiedSlots = (int) (STATION_OCCUPIED_PERCENTAGE * numberOfSlots);
+
+        int numberOfFreeSlots = numberOfSlots - numberOfOccupiedSlots;
+
         // Percentage of electrical bicycle
         double percentageOfElectricalBicycle = PERCENTAGE_OF_ELECTRICAL_BICYCLE;
+
+        double percentageOfPlusStations = PERCENTAGE_OF_PLUS_STATIONS;
+
+        int numberOfPlusStations = (int) (PERCENTAGE_OF_PLUS_STATIONS * numberOfStations);
+
+        int numberOfStandardStations = numberOfStations - numberOfPlusStations;
+
+        int numberOfElectricalBicycleSlots = (int) (PERCENTAGE_OF_ELECTRICAL_BICYCLE * numberOfOccupiedSlots);
+
+        int numberOfMechanicalBicycleSlots = numberOfOccupiedSlots - numberOfElectricalBicycleSlots;
 
         // Area of the myVelib system
         double squareSide = Double.parseDouble(args[3]);
@@ -39,10 +63,41 @@ public class InitialSetup {
 
 
         // Creating of input parameters to createMyVelibSystem
-        // ALGOS
 
-        // Creation of the myVelib system
-        MyVelibSystem system0 = MyVelibSystemFactory.createMyVelibSystem();
+        // parkingSlots
+        List<ParkingSlot> electricalSlots = ParkingSlotsFactory.createElectricalBicycleSlots(numberOfElectricalBicycleSlots);
+        List<ParkingSlot> mechanicalSlots = ParkingSlotsFactory.createElectricalBicycleSlots(numberOfMechanicalBicycleSlots);
+        List<ParkingSlot> freeSlots = ParkingSlotsFactory.createFreeParkingSlots(numberOfFreeSlots);
+        List<ParkingSlot> allSlots = Stream.concat(freeSlots.stream(), mechanicalSlots.stream()).collect(Collectors.toList());
+        allSlots = Stream.concat(allSlots.stream(), electricalSlots.stream()).collect(Collectors.toList());
+
+        // stations
+        List<Station> stations = new ArrayList<Station>();
+
+        for (int i = 0; i < numberOfPlusStations; i++) {
+            // Create the point to random // TODO: Learn know to do the random stuff
+            // TODO: Create a test to check if there is already a station when creating one
+
+            // Create the plus stations
+            Station station = StationFactory.addPlusStation(system0, new Point(0, 0), true, allSlots);
+
+        }
+
+        for (int i = 0; i < numberOfStandardStations; i++) {
+            // Create the point to random // TODO: Learn know to do the random stuff
+            // TODO: Create a test to check if there is already a station when creating one
+
+            // Create the standard stations
+            Station station = StationFactory.addStandardStation(system0, new Point(0, 0), true, allSlots);
+
+        }
+
+
+
+
+
+
+
 
 
 
@@ -58,7 +113,7 @@ public class InitialSetup {
         CardFactory.addVlibre(system0, user2);
 
 
-
+        System.out.println(system0);
 
     }
 
