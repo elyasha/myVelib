15bfb588c4b5437e9e73a606586ac147602b9326
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-29 22:56:45 +0200

    Updated initial setup and user factory

--- src/core/components/factories/UserFactory.java
+++ src/core/components/factories/UserFactory.java
@@ -1,5 +1,6 @@
 package core.components.factories;
 
+import core.components.Card;
 import core.components.Manager;
 import core.components.User;
 import core.system.MyVelibSystem;
@@ -9,32 +10,37 @@ import java.awt.*;
 
 public class UserFactory {
 
-    public void addUser(MyVelibSystem system, String name, Point coordinate, int creditCardNumber, double money) {
+    public static User addUser(MyVelibSystem system, String name, Point coordinate, int creditCardNumber, double money) {
         User user = new User(name, coordinate, creditCardNumber, money);
         List<User> users = system.getUsers();
         users.add(user);
         system.setUsers(users);
+        return user;
     }
 
-    public void addUser(MyVelibSystem system, String name, int creditCardNumber) {
+    public static User addUser(MyVelibSystem system, String name, int creditCardNumber) {
         User user = new User(name, new Point(0, 0), creditCardNumber, 0);
         List<User> users = system.getUsers();
         users.add(user);
         system.setUsers(users);
+        return user;
     }
 
-    public void addManager(MyVelibSystem system, String name, Point coordinate, int creditCardNumber, double money) {
+
+    public static Manager addManager(MyVelibSystem system, String name, Point coordinate, int creditCardNumber, double money) {
         Manager user = new Manager(name, coordinate, creditCardNumber, money);
         List<User> users = system.getUsers();
         users.add(user);
         system.setUsers(users);
+        return user;
     }
 
-    public void addManager(MyVelibSystem system, String name, int creditCardNumber) {
+    public static Manager addManager(MyVelibSystem system, String name, int creditCardNumber) {
         Manager user = new Manager(name, new Point(0, 0), creditCardNumber, 0);
         List<User> users = system.getUsers();
         users.add(user);
         system.setUsers(users);
+        return user;
     }
 
 
--- src/core/setup/InitialSetup.java
+++ src/core/setup/InitialSetup.java
@@ -1,23 +1,65 @@
 package core.setup;
 
+import core.components.User;
+import core.components.factories.CardFactory;
+import core.components.factories.UserFactory;
+import core.system.MyVelibSystem;
+import core.system.MyVelibSystemFactory;
+
+import java.awt.*;
+import java.util.Scanner;
+
 public class InitialSetup {
 
-    // Creation of the myVelib system
+    private static final double STATION_OCCUPIED_PERCENTAGE = 0.7;
+    private static final double PERCENTAGE_OF_ELECTRICAL_BICYCLE = 0.3;
 
-    // Quantity of stations
+    public static void main(String[] args) {
 
-    // Quantity of parking slots
+        // Name of the myVelib system
+        String name = args[0];
 
-    // Station occupied percentage
+        // Quantity of stations
+        int numberOfStations = Integer.parseInt(args[1]);
 
-    // Percentage of electrical bicycle
+        // Quantity of parking slots
+        int numberOfSlots = Integer.parseInt(args[2]);
 
-    // Area of the myVelib system
+        // Station occupied percentage
+        double stationOccupiedPercentage = STATION_OCCUPIED_PERCENTAGE;
 
-    // Add User without card
+        // Percentage of electrical bicycle
+        double percentageOfElectricalBicycle = PERCENTAGE_OF_ELECTRICAL_BICYCLE;
 
-    // Add User with Vmax
+        // Area of the myVelib system
+        double squareSide = Double.parseDouble(args[3]);
 
-    // Add User with Vlibre
+        // Number of bicycles
+        int numberOfBicycles = Integer.parseInt(args[4]);
+
+
+        // Creating of input parameters to createMyVelibSystem
+        // ALGOS
+
+        // Creation of the myVelib system
+        MyVelibSystem system0 = MyVelibSystemFactory.createMyVelibSystem();
+
+
+
+        // Add User without card
+        UserFactory.addUser(system0,"Matheus" , new Point(0, 0),30109903, 100000000);
+
+        // Add User with Vmax
+        User user = UserFactory.addUser(system0, "Charlito", new Point(1, 1), 192340, 0);
+        CardFactory.addVmax(system0, user);
+
+        // Add User with Vlibre
+        User user2 = UserFactory.addUser(system0, "Charlito2", new Point(1, 1), 192340, 0);
+        CardFactory.addVlibre(system0, user2);
+
+
+
+
+    }
 
 }
