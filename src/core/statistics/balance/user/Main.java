6ba164b65cbf5321fb5ac8ef5dc8eb66006eea33
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-31 13:59:30 +0200

    Initial work on statistics for user

--- src/core/statistics/balance/user/Main.java
+++ src/core/statistics/balance/user/Main.java
@@ -7,13 +7,52 @@ public class Main {
 
     public static void userBalance(MyVelibSystem system) {
 
-        // Calculate de average user balance
+        // Calculate the average user balance
+        double averageUserBalance = 0; // Initialize the average to zero
+
+        for (User user: system.getUsers()
+             ) {
+            averageUserBalance += user.getMoney();
+        }
+        averageUserBalance /= system.getUsers().size();
+
+        System.out.println("The average user balance is: " + averageUserBalance + "!");
+        System.out.println(); // skip line
+
 
         // Calculate the average of all charged money
+        double averageOfAllChargedMoney = 0;
+
+        for (User user: system.getUsers()
+             ) {
+            averageOfAllChargedMoney += user.getAllMoneyCharged();
+        }
+
+        averageOfAllChargedMoney /= system.getUsers().size();
+
+        System.out.println("The average of all charged money is: " + averageOfAllChargedMoney + "!");
+        System.out.println();
+
 
         // Calculate the user that paid the most to the myVelib system
+        User userWhoPaidTheMost = system.getUsers().get(0); // Take the first user
+
+        for (User user: system.getUsers()
+             ) {
+
+            // If user has a higher allChargedMoney parameter, we change
+            if (user.getAllMoneyCharged() > userWhoPaidTheMost.getAllMoneyCharged()) {
+                userWhoPaidTheMost = user;
+            }
+
+        }
+
+        System.out.println("User who paid the most to the system is: " + userWhoPaidTheMost + "!");
+        System.out.println();
 
         // Calculate other stuff
+
+        // TODO: (Charlito) See if we can add more stuff here and maybe refactor the methods
 
     }
 }
