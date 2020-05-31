519214d3af2961eac29041853a4f76053c5ce484
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-31 20:55:31 +0200

    Updated AddManager command

--- src/cli/commands/AddManager.java
+++ src/cli/commands/AddManager.java
@@ -12,4 +12,29 @@ public class AddManager implements Command {
     public static void main(String[] args) {
         System.out.println("The addUser command!");
     }
+
+    public boolean hasGoodInput(String[] args) {
+        // Check the quantity of arguments in this command
+        if (args.length != 3){
+            return false;
+        }
+        else{
+
+            // Check if the cardType is well written
+            if (!args[1].equals("Vmax") && !args[1].equals("Vlibre")) {
+                CardTypeError();
+                return false;
+            }
+            else {
+                return true;
+            }
+
+
+        }
+    }
+
+    private void CardTypeError() {
+        System.out.println("There is an error in the card type!");
+        System.out.println("Please, add a coherent card type (Vmax, Vlibre, none)");
+    }
 }
