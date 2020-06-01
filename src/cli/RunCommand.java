22cc189a0468d1b19b804fb2cc8cc9e3f8a44fb0
Matheus Elyasha <matheuselyasha@gmail.com>
2020-06-02 01:34:07 +0200

    updated runTest command

--- src/cli/RunCommand.java
+++ src/cli/RunCommand.java
@@ -20,60 +20,61 @@ public class RunCommand {
         // Check if there is any argument for the program
         if (args.length == 0) {
             Help.wrongArgumentHelp();
-            System.exit(1);
+        }
+        else {
+            String[] argsCommand = new String[args.length - 1];
+
+            for (int j = 1; j < args.length; j++) {
+                argsCommand[j - 1] = args[j];
+            }
+            // Check the type of first argument and delegate the work for each class
+            switch (args[0]) {
+
+                case "addUser":
+                    AddUser.main(argsCommand);
+                    break;
+                case "display":
+                    Display.main(argsCommand);
+                    break;
+                case "displayStation":
+                    DisplayStation.main(argsCommand);
+                    break;
+                case "displayUser":
+                    DisplayUser.main(argsCommand);
+                    break;
+                case "exit":
+                    Exit.main(argsCommand);
+                    break;
+                case "help":
+                    Help.main(argsCommand);
+                    break;
+                case "offline":
+                    Offline.main(argsCommand);
+                    break;
+                case "online":
+                    Online.main(argsCommand);
+                    break;
+                case "rentBicycle":
+                    RentBicycle.main(argsCommand);
+                    break;
+                case "returnBicycle":
+                    ReturnBicycle.main(argsCommand);
+                    break;
+                case "runTest":
+                    RunTest.main(argsCommand);
+                    break;
+                case "setup":
+                    Setup.main(argsCommand);
+                    break;
+                case "sortStation":
+                    SortStation.main(argsCommand);
+                    break;
+                default:
+                    System.out.println("Not the good usage of the myVelib program. Please see the documentation [myvelib help]");
+            }
         }
 
-        String[] argsCommand = new String[args.length - 1];
 
-        for (int j = 1; j < args.length; j++) {
-            argsCommand[j - 1] = args[j];
-        }
-        // Check the type of first argument and delegate the work for each class
-        switch (args[0]) {
-
-            case "addUser":
-                AddUser.main(argsCommand);
-                break;
-            case "display":
-                Display.main(argsCommand);
-                break;
-            case "displayStation":
-                DisplayStation.main(argsCommand);
-                break;
-            case "displayUser":
-                DisplayUser.main(argsCommand);
-                break;
-            case "exit":
-                Exit.main(argsCommand);
-                break;
-            case "help":
-                Help.main(argsCommand);
-                break;
-            case "offline":
-                Offline.main(argsCommand);
-                break;
-            case "online":
-                Online.main(argsCommand);
-                break;
-            case "rentBicycle":
-                RentBicycle.main(argsCommand);
-                break;
-            case "returnBicycle":
-                ReturnBicycle.main(argsCommand);
-                break;
-            case "runTest":
-                RunTest.main(argsCommand);
-                break;
-            case "setup":
-                Setup.main(argsCommand);
-                break;
-            case "sortStation":
-                SortStation.main(argsCommand);
-
-                break;
-            default:
-                System.out.println("Not the good usage of the myVelib program. Please see the documentation [myvelib help]");
-        }
 
     }
 }
