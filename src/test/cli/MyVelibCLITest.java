4167a7184063cf5934409845f2fd78725615966c
Baudinet Charles <ch.baudinet@gmail.com>
2020-06-03 23:08:47 +0200

    finished CLI tests

---
+++ src/test/cli/MyVelibCLITest.java
@@ -0,0 +1,22 @@
+package cli;
+
+import junit.framework.TestCase;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static cli.MyVelibCLI.readTextFile;
+
+public class MyVelibCLITest extends TestCase {
+
+    public void testMain() {
+    }
+
+    public void testReadTextFile() {
+        String filename = "src\\test\\cli\\testReadFileName.txt";
+        List<String> result = Arrays.asList("myVelib");
+        assertEquals(result, readTextFile(filename));
+    }
+}
