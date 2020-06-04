24ba6c844b42e92301ca8b859e0ef2574bd19a43
Baudinet Charles <ch.baudinet@gmail.com>
2020-06-04 10:08:36 +0200

    Finalized Terminal tests

--- src/test/core/components/TerminalTest.java
+++ src/test/core/components/TerminalTest.java
@@ -1,6 +1,16 @@
 package core.components;
 
+import core.Main;
+import core.components.factories.BicycleFactory;
+import core.components.factories.ParkingSlotsFactory;
+import core.components.factories.PointFactory;
+import core.components.factories.UserFactory;
+import core.system.MyVelibSystem;
+import core.system.MyVelibSystemFactory;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,13 +18,35 @@ public class TerminalTest {
 
     @Test
     public void rentBicycle() {
+        List<ParkingSlot> listPark = ParkingSlotsFactory.createElectricalBicycleSlots(10);
+        PlusStation station = new PlusStation(PointFactory.addPoint(0,0),true,listPark,100,0);
+        Terminal term = station.getStationTerminal();
+        List<Station> stations = new ArrayList<Station>();
+        stations.add(station);
+        MyVelibSystemFactory.createMyVelibSystem(stations);
+        List<MyVelibSystem> systems = new ArrayList<MyVelibSystem>();
+        systems = Main.getSystems();
+        User user = UserFactory.addUser(systems.get(0),"Charlito",PointFactory.addPoint(0,0),999,200);
+        Bicycle bicycle = BicycleFactory.createElectricalBicycle();
+        term.rentBicycle(bicycle,user,systems.get(0).getStations().get(0));
+        assertTrue(user.getBicycle()!=null);
     }
 
-    @Test
-    public void dropBicycleWithIntervalOfTime() {
-    }
 
     @Test
     public void dropBicycle() {
+        List<ParkingSlot> listPark = ParkingSlotsFactory.createElectricalBicycleSlots(10);
+        PlusStation station = new PlusStation(PointFactory.addPoint(0,0),true,listPark,100,0);
+        Terminal term = station.getStationTerminal();
+        List<Station> stations = new ArrayList<Station>();
+        stations.add(station);
+        MyVelibSystemFactory.createMyVelibSystem(stations);
+        List<MyVelibSystem> systems = new ArrayList<MyVelibSystem>();
+        systems = Main.getSystems();
+        User user = UserFactory.addUser(systems.get(0),"Charlito",PointFactory.addPoint(0,0),999,200);
+        Bicycle bicycle = BicycleFactory.createElectricalBicycle();
+        term.rentBicycle(bicycle,user,systems.get(0).getStations().get(0));
+        term.dropBicycle(bicycle,user,systems.get(0).getStations().get(0));
+        assertTrue(user.getBicycle()==null);
     }
 }
