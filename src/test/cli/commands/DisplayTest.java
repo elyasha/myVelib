8a60360bd014c73c3c3023537cb31458053ebc39
Baudinet Charles <ch.baudinet@gmail.com>
2020-06-05 14:00:11 +0200

    Doc DisplayTest

--- src/test/cli/commands/DisplayTest.java
+++ src/test/cli/commands/DisplayTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import static cli.commands.Display.hasGoodInput;
 import static org.junit.Assert.*;
 
+/**
+ * Testing DisplayTest Class
+ */
 public class DisplayTest {
 
     @Test
@@ -12,6 +15,9 @@ public class DisplayTest {
         //TODO
     }
 
+    /**
+     * Testing good input arguments
+     */
     @Test
     public void hasGoodInput1() {
         //[velibnetworkName]
@@ -19,6 +25,9 @@ public class DisplayTest {
         assertEquals(true, hasGoodInput(args));
     }
 
+    /**
+     * Testing bad input arguments
+     */
     @Test
     public void hasGoodInput2() {
         //[velibnetworkName]
